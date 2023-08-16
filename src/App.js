771b7f76b7a457deb77efa0ebfe636238e8bbfb8
Akarsh Jain <akarsh.jain.790@gmail.com>
2023-08-16 19:32:57 +0530

    ui: Add leftside bar and navbar.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import SignIn from "./Pages/Accounts/SignIn";
+import HomePage from "./Pages/Home/HomePage";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<HomePage />} />
           <Route path="/sign-in" element={<SignIn />} />
         </Routes>
       </BrowserRouter>
---
+++ src/Pages/Home/HomePage.jsx
@@ -0,0 +1,12 @@
+import LeftSideBar from "../Components/LeftSideBar";
+import NavBar from "../Components/NavBar";
+
+
+export default function () {
+    return (
+        // <div class="container-fluid page-body-wrapper">
+        <LeftSideBar />
+        // {/* <NavBar /> */ }
+        // </div>
+    );
+}
